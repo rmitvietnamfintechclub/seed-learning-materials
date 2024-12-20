994e24ec09384a2436cd338b36bcd0c4c5e816bc
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-12-20 11:46:21 +0700

    Update documentation link

--- JAVASCRIPT/Color Picker/script.js
+++ JAVASCRIPT/Color Picker/script.js
@@ -1,3 +1,5 @@
+// All Documentations: https://rmiteduau.sharepoint.com/:f:/r/sites/RMITFinTechClub2024/Shared%20Documents/RMIT%20FinTech%20Club%20Master%20Folder/Semester%20C/001.%20Departments/003.%20Technology/002.%20Department%20Projects/003.%20SEED%20Training%20Project/002.%20Documentation?csf=1&web=1&e=G9hO2W
+
 // Step 1: Get DOM elements
 
 // Function to generate a random color
