19bcc52c05f214ff91315ad362eb818b5a57ce0d
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-12-20 09:54:47 +0700

    Update API for Weather Application

---
+++ JAVASCRIPT/Weather Application/script.js
@@ -0,0 +1,6 @@
+// Used these APIs for your weather application:
+// API for weather data: https://geocoding-api.open-meteo.com/v1/search?name=paris
+// API for location: https://api.open-meteo.com/v1/forecast?latitude=13.41053&longitude=52.52437&current_weather=true
+
+// Weather API Docs: https://open-meteo.com/en/docs
+// Location API Docs: https://open-meteo.com/en/docs/geocoding-api
